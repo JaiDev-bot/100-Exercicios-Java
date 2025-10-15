02c3a90317506af4158ea8eeabe94634f331a6be
Jaiane <jaianesoares700@gmail.com>
2025-10-15 17:49:18 -0300

    feat:segundo ex

--- src/logica/Exercicio2.java
+++ src/logica/Exercicio2.java
@@ -1,4 +1,25 @@
 package logica;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Digite seu nome: ");
+        String name = scanner.next();
+
+        System.out.println("Olá " + name + ", é um prazer te conhecer! ") ;
+
+
+
+
+
+    }
+
+
+
+
 }
