92b9e844b8a30d0372260b2915ec19b71953625c
Jaiane <jaianesoares700@gmail.com>
2025-10-15 17:58:34 -0300

    feat:terceiro ex

---
+++ src/logica/Exercicio3.java
@@ -0,0 +1,28 @@
+package logica;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            System.out.println("Qual o seu nome? ");
+            String name = scanner.next();
+
+            System.out.println("Qual o seu salário, " + name + "?");
+            double salario = scanner.nextDouble();
+
+            System.out.println(" O funcionário " + name+ " recebe o sálario de "+ salario);
+
+
+
+        } catch (InputMismatchException e ) {
+            System.out.println("Error: digite um numero ");
+        }
+
+
+
+    }
+}
