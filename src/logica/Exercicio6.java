f0544868460748fddcd43fbc69aad0121bf5ec3d
Jaiane <jaianesoares700@gmail.com>
2025-10-15 18:55:44 -0300

    feat:sexto ex

---
+++ src/logica/Exercicio6.java
@@ -0,0 +1,28 @@
+package logica;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num;
+
+        try{
+            System.out.println("Digite um  um numero: ");
+            num = scanner.nextInt();
+
+            int sucessor = num + 1;
+            int antecessor = num - 1;
+
+            System.out.println("O sucessor de " + num + " é " + sucessor);
+            System.out.println("O antecessor de " + num + " é " + antecessor);
+
+        }catch (InputMismatchException e) {
+            System.out.println("error: digite um numero");
+
+        }
+    }
+
+}
